08e5d70c7abff26b1d4ac6f626f5f7e88d8e3706
諒弘 銭谷 <masahirozenitani@gmail.com>
2025-09-12 00:28:50 +0900

    C#

--- Program.cs
+++ Program.cs
@@ -5,57 +5,73 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace inheritance
-{
-    internal class Program
+{ 
+// 基底クラス
+class TestPerson
     {
-        class TestPerson
-        {// 基底クラス
-            public string Name { get; set; }
-            public int Age { get; set; }
-
-            public TestPerson(string name, int age)
-            {
-                Name = name;
-                Age = age;
-
-                // コンソール出力処理1
-                Console.WriteLine("基底クラス");
-                Console.WriteLine($"名前:{Name},年齢:{Age}");
-            }
-        }
-
-        // 派生クラス
-        class TestInfo : TestPerson
-        {
-            public int Height { get; set; }
-            public int Weight { get; set; }
-
-            public TestInfo(string name, int age, int height, int weight)
-                : base(name, age)
-            {
-                Height = height;
-                Weight = weight;
-
-                Console.WriteLine("派生クラス");
-                Console.WriteLine($"身長:{Height},体重:{Weight}");
-            }
-        }
-
-
-
-
-        class s
-        {
-            static void Main(string[] args)
-            {
-                // 派生クラスのインスタンス生成
-                TestInfo s = new TestInfo("山田", 20, 170, 60);
-
-                // メソッド呼び出し
-
-            }
-        }
+        protected string name;
+        protected string age;
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 
 
